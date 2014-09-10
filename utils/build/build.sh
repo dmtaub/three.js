15404c6956a3346917d31d2769b5da14d39e9867
Daniel Taub <dan@cemi.org>
2014-09-10 15:33:15 -0400

    adding build script that compiles math

--- utils/build/build.sh
+++ utils/build/build.sh
@@ -2,4 +2,5 @@
 
 cd "$(dirname "$0")"
 python build.py --include common --include extras --output ../../build/three.js
+python build.py --include math --output ../../build/three-math.js
 python build.py --include common --include extras --minify --output ../../build/three.min.js
